966d041f27b7bd98d6f9ab8daa199415f7cc29cd
Nitin Rajput <65024943+nitinrajput1521@users.noreply.github.com>
2024-03-01 12:53:44 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,135 @@
+function loco() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+    smoothMobile: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+loco();
+
+gsap.to("#page>viedo", {
+  scrollTrigger: {
+    trigger: "#page>video",
+    start: `10% top`,
+    end: `bottom top`,
+    scroller: `#main`,
+    scrub:4,
+  },
+  onStart: () => {
+    document.querySelector("#page>video").play();
+  },
+});
+
+gsap.to("#page", {
+  scrollTrigger: {
+    trigger: "#page",
+    start: `top top`,
+    end: `bottom top`,
+    scroller: `#main`,
+    //scrub:true,
+    //markers:true,
+    pin:true
+  }
+});
+gsap.to("#page>#nav-container",{
+ scrollTrigger:{
+   trigger:"#page>#nav-container",
+   start:`7% top`,
+   end:`bottom top`,
+   scroller:`#main`,
+   //markers:true,
+ },
+top:`-7%`,
+duration:1,
+scrub:2,
+});
+gsap.to("#page-bottom", {
+  scrollTrigger: {
+    trigger: `#page-bottom`,
+    start: `top top`,
+    end: `15% top`,
+    scroller: `#main`,
+    //markers:true,
+    scrub:2,
+  },
+  opacity: 0,
+});
+
+var tl = gsap.timeline({
+  scrollTrigger:{
+    trigger: "#page1",
+    start: `top top`,
+    scrub:1,
+    scroller: `#main`,
+    //markers:true,
+    pin:true
+  }
+})
+
+tl.to("#page1>h1",{
+  top:`-50%`,
+}
+)
+var tl1 = gsap.timeline({
+  scrollTrigger:{
+    trigger: "#page2",
+    start: `top top`,
+    
+    scrub:1,
+    scroller: `#main`,
+    //markers:true,
+    pin:true
+  }
+})
+
+tl1.to("#page2>h1",{
+  top:`-50%`,  
+}
+)
+var tl2 = gsap.timeline({
+  scrollTrigger:{
+    trigger: "#page4",
+    start: `top top`,
+    scrub:1,
+    scroller: `#main`,
+    //markers:true,
+    pin:true
+  }
+})
+
+tl2.to("#page4>#center-page4",{
+  top:`-50%`,  
+}
+)
